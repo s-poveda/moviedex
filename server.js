a59df13b579ad02efb55d9a6a9575f1ebd3055ea
nickjang <nickjang@alumni.upenn.edu>
2020-10-20 15:44:30 -0400

    Clean up code, Add to authorization

--- server.js
+++ server.js
@@ -6,38 +6,50 @@ const cors = require('cors');
 const Data = require('./data.json');
 const TOKEN = process.env.API_TOKEN;
 
-
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(authorize);
 
-function authorize (req, res, next) {
-	const header = req.get('Authorization') || '';
-	const token = header.split(' ')[1];
-	if (token === TOKEN) return next();
+/**
+ * Validate API Bearer token.
+ */
+function authorize(req, res, next) {
+  const header = req.get('Authorization') || '';
+  const token = header.split(' ')[1];
+  if (token === TOKEN) return next();
 
-	res.status(401).end();
+  res.status(401).json({
+    error: 'Please give a valid API Token.'
+  });
 }
 
-app.get('/movie', (req,res) => {
-	const { genre, country, avg_vote } = req.query;
-	let body = Data;
+/**
+ * Filter by search parameters genre,
+ * country or average vote.
+ */
+function handleGetMovies(req, res) {
+  const { genre, country, avg_vote } = req.query;
+  let body = Data;
 
-	if (genre) {
-		body = body.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
-	}
+  if (genre) {
+    body = body.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+  }
 
-	if (country) {
-		body = body.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
-	}
+  if (country) {
+    body = body.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
+  }
 
-	if (avg_vote) {
-		body = body.filter(movie => movie.avg_vote >= Number(avg_vote));
-	}
+  if (avg_vote) {
+    body = body.filter(movie => movie.avg_vote >= Number(avg_vote));
+  }
 
-	res.json(body);
-});
+  res.json(body);
+}
 
-app.listen(8000, () => 'Server listening at http://localhost:8000.');
+
+app.get('/movie', handleGetMovies);
+
+const PORT=8000;
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}.`));
